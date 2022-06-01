8d07bb12bd19878fa51a1e6da7362a3ec8f18a09
4R4S8 <arashzomorrodi24@gmail.com>
2022-06-01 20:31:23 +0430

    Average

--- FCFS/Main/Program.cs
+++ FCFS/Main/Program.cs
@@ -4,6 +4,9 @@ int x = int.Parse(Console.ReadLine());
 int at = 0;
 int bt = 0;
 
+double tt = 0;
+double wt = 0;
+
 int[] Finish_Time = new int[x];
 int[] Arrival_Time = new int[x];
 int[] Burst_Time = new int[x];
@@ -37,10 +40,11 @@ for (int j = 0; j < Arrival_Time.Length; j++)
 
     Waiting_Time[j]=Turn_Around[j]-Burst_Time[j];
 
+    tt += Turn_Around[j];
+    wt += Waiting_Time[j];
 }
-
-//Console.WriteLine(Finish_Time[0]);
-
+tt/=x;
+wt/=x;
 Console.WriteLine();
 
 Console.WriteLine("Process\t Arrival Time\t Burst Time\t Finish Time\t Turnaround Time\t Waiting Time");
@@ -50,6 +54,7 @@ Console.WriteLine();
 for (int i = 0; i < x; i++)
 {
     Console.WriteLine("P{0}\t\t{1}\t\t{2}\t\t{3}\t\t{4}\t\t{5}",i+1,Arrival_Time[i],Burst_Time[i],Finish_Time[i],Turn_Around[i],Waiting_Time[i]);
-
     Console.WriteLine();
 }
+
+System.Console.WriteLine("TT Avg = {0}   WT Avg = {1}",tt,wt);
