e6ff4beeb8ab34692e77a475d00e83591f71c042
4R4S8 <arashzomorrodi24@gmail.com>
2022-06-01 20:04:08 +0430

    Finish time

---
+++ FCFS/Main/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.Write("Enter The Length of the Array: ");
+
+int x = int.Parse(Console.ReadLine());
+int at = 0;
+int bt = 0;
+
+int[] Finish_Time=new int[x];
+int[] Arrival_Time=new int[x];
+int[] Burst_Time=new int[x];
+
+Console.WriteLine();
+
+for (int j = 0; j < Arrival_Time.Length; j++)
+{
+    
+    Console.WriteLine();
+    Console.Write("P{0} Arrival Time: ",j+1);
+    at = int.Parse(Console.ReadLine());
+    Arrival_Time[j] = at;
+
+    Console.Write("P{0} Burst Time: ",j+1);
+    bt = int.Parse(Console.ReadLine());
+    Burst_Time[j] = bt;
+
+    if (j==0)
+    {
+        Finish_Time[j]=Arrival_Time[j]+Burst_Time[j];
+    }
+    else
+    {
+        Finish_Time[j]=Finish_Time[j-1]+Burst_Time[j];
+    }
+}
+
+//Console.WriteLine(Finish_Time[0]);
+
+Console.WriteLine();
+
+Console.WriteLine("Process\t Arrival Time\t Burst Time\t Finish Time\t Turnaround Time\t Waiting Time");
+
+Console.WriteLine();
+
+for (int i = 0; i < x; i++)
+{
+    Console.WriteLine("P{0}\t{1}\t\t{2}\t\t{3}",i+1,Arrival_Time[i],Burst_Time[i],Finish_Time[i]);
+
+    Console.WriteLine();
+    
+}
---
+++ Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine("Chose a Process Scheduler Algorithmc :\n[0] FCFS\n[1] SJF\n[2] SRTF\n[3] RR");
